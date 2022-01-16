deb4630d7c08c1afe659853ff66614b5049b9732
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-01-16 11:09:09 +0100

    Updates

---
+++ precache-manifest.1115a616a1c5b8cb867d2f0aa949f3eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e43d4e45616376e202b3e9cf5161625",
+    "url": "/waveportal/index.html"
+  },
+  {
+    "revision": "c9a23f926468650783ed",
+    "url": "/waveportal/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/waveportal/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "c9a23f926468650783ed",
+    "url": "/waveportal/static/js/main.1696b03a.chunk.js"
+  },
+  {
+    "revision": "aef81ca1c527b8d66c72",
+    "url": "/waveportal/static/js/runtime-main.4df9bcaa.js"
+  }
+]);
